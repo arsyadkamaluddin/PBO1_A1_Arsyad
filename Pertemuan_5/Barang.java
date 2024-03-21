3eae09e4db6acb1d69e03abfdacb3cb9b482ce43
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-03-21 13:42:15 +0700

    Sixth task's template  added

--- Pertemuan_5/Barang.java
+++ Pertemuan_5/Barang.java
@@ -1,31 +1,39 @@
-import java.util.ArrayList;
-
 public class Barang {
     private String namaBarang;
-    private int hargaBarang, jumlahBarang;
+    private int hargaBarang;
+    private int jumlahBarang;
 
-    public Barang(String namaBarang, int hargaBarang, int jumlahBarang) {
+    public Barang(String nama, int harga, int jml) {
+        this.namaBarang = nama;
+        this.hargaBarang = harga;
+        this.jumlahBarang = jml;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
         this.namaBarang = namaBarang;
+    }
+
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+    public void setHargaBarang(int hargaBarang) {
         this.hargaBarang = hargaBarang;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
         this.jumlahBarang = jumlahBarang;
     }
 
-    public void setNamaBarang(String nama){
-        this.namaBarang= nama;
-    }
-    public String getNamaBarang(){
-        return this.namaBarang;
-    }
-    public void setHargaBarang(int harga){
-        this.hargaBarang=harga;
-    }
-    public int getHargaBarang(){
-        return this.hargaBarang;
-    }
-    public void setJumlahBarang(int jumlah){
-        this.jumlahBarang=jumlah;
-    }
-    public int getJumlahBarang(){
-        return this.jumlahBarang;
+    public int totalHarga() {
+        return hargaBarang * jumlahBarang;
     }
 }
