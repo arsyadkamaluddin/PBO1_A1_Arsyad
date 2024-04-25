5cd3255c7fbf138aa10f562951fc2bd74e30fdc4
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-04-25 14:46:09 +0700

    d

--- UTS/TravelAgent.java
+++ UTS/TravelAgent.java
@@ -26,16 +26,76 @@ public class TravelAgent {
         }
         return count;
     }
-    
+
+    private int countAvailableDate(String tanggal) {
+        int count = 0;
+        for (Trip trip : trips) {
+            if (trip.getDeparture() == tanggal) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int countAvailableType(TripType tripType) {
+        int count = 0;
+        for (Trip trip : trips) {
+            if (trip.getType() == tripType) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public void showAvailableTrip() {
         System.out.println("Terdapat " + countAvailable() + " trip yang tersedia :");
         for (Trip trip : trips) {
-            if (trip.getQty() > 0){
+            if (trip.getQty() > 0) {
                 System.out.println("Destinasi : " + trip.getDestination() + " -- Keberangkatan : " + trip.getDeparture()
                         + "-- Harga : " + trip.getPrice() + " -- Qty : " + trip.getQty() + " -- Jenis : "
                         + trip.getType());
             }
         }
+    }
+
+    public void getAvailableTripsByType(TripType tripType) {
+        System.out.println("Ditemukan " + countAvailableType(tripType) + " trip untuk jenis" + tripType.toString());
+        for (Trip trip : trips) {
+            if (trip.getType() == tripType) {
+                System.out.println("Destinasi : " + trip.getDestination() + " -- Keberangkatan : " + trip.getDeparture()
+                        + "-- Harga : " + trip.getPrice() + " -- Qty : " + trip.getQty() + " -- Jenis : "
+                        + trip.getType());
+            }
+        }
+    }
+
+    public void getAvailableTripsByDate(String tanggal) {
+        System.out.println("Ditemukan " + countAvailableDate(tanggal) + " trip untuk tanggal" + tanggal);
+        for (Trip trip : trips) {
+            if (trip.getDeparture() == tanggal) {
+                System.out.println("Destinasi : " + trip.getDestination() + " -- Keberangkatan : " + trip.getDeparture()
+                        + "-- Harga : " + trip.getPrice() + " -- Qty : " + trip.getQty() + " -- Jenis : "
+                        + trip.getType());
+            }
+        }
+
+    }
+
+    public void getBookingsByCustomerEmail(String email) {
+        Boolean exist = false;
+        System.out.println("Pesanan dengan email " + email);
+        for (Book book : books) {
+            if (book.getEmail() == email) {
+                exist = true;
+                System.out.println("Booking ID : " + book.getId() + ", Destinasi : " + book.getTrip());
+            }
+        }
+        if (!exist)
+            System.out.println("Tidak ada");
+    }
+
+    private String generateBookingID(Trip trip) {
+        return String.format("%s%s%d", trip.getDestination(), trip.getType().toString(), books.size() + 1);
     }
 
     public void bookTrip(Customer customer, Trip trip) {
@@ -64,10 +124,6 @@ public class TravelAgent {
         System.out.println("Pesanan dengan ID Booking " + idBooking + " Berhasil ditambahkan");
     }
 
-    private String generateBookingID(Trip trip) {
-        return String.format("%s%s%d", trip.getDestination(), trip.getType().toString(), books.size() + 1);
-    }
-
     public void cancelBooking(String email, Trip trip) {
         int ind = -1;
         int stat = 0;
@@ -89,55 +145,5 @@ public class TravelAgent {
             return;
         }
         System.out.println("Tidak ditemukan booking yang sesuai");
-    }
-    public void getBookingsByCustomerEmail(String email){
-        Boolean exist = false;
-        System.out.println("Pesanan dengan email "+email);
-        for(Book book : books){
-            if(book.getEmail()==email){
-                exist = true;
-                System.out.println("Booking ID : "+book.getId()+", Destinasi : "+book.getTrip());
-            }
-        }
-        if(!exist)System.out.println("Tidak ada");
-    }
-    public void getAvailableTripsByType(TripType tripType){
-        System.out.println("Ditemukan "+countAvailableType(tripType)+" trip untuk jenis"+tripType.toString());
-        for(Trip trip:trips){
-            if(trip.getType()==tripType){
-                System.out.println("Destinasi : " + trip.getDestination() + " -- Keberangkatan : " + trip.getDeparture()
-                        + "-- Harga : " + trip.getPrice() + " -- Qty : " + trip.getQty() + " -- Jenis : "
-                        + trip.getType());
-            }
-        }
-    }
-    private int countAvailableType(TripType tripType) {
-        int count = 0;
-        for (Trip trip : trips) {
-            if (trip.getType() == tripType) {
-                count++;
-            }
-        }
-        return count;
-    }
-    private int countAvailableDate(String tanggal) {
-        int count = 0;
-        for (Trip trip : trips) {
-            if (trip.getDeparture() == tanggal) {
-                count++;
-            }
-        }
-        return count;
-    }
-    public void getAvailableTripsByDate(String tanggal){
-        System.out.println("Ditemukan "+countAvailableDate(tanggal)+" trip untuk tanggal "+tanggal);
-        for(Trip trip:trips){
-            if(trip.getDeparture() == tanggal){
-                System.out.println("Destinasi : " + trip.getDestination() + " -- Keberangkatan : " + trip.getDeparture()
-                        + "-- Harga : " + trip.getPrice() + " -- Qty : " + trip.getQty() + " -- Jenis : "
-                        + trip.getType());
-            }
-        }
-
     }
 }
